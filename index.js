40ecad1965d59fb06d428319ac30945d32f51ebe
jordanrios94 <jordanrios.software@gmail.com>
2019-03-15 15:50:52 +0000

    fix: app port binding

--- index.js
+++ index.js
@@ -11,3 +11,6 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
